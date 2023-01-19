045ebe82745feb6658215d36a4e3a27ea3627de9
Tyler M. Neher <tyler@neherdata.com>
2023-01-19 10:43:54 -0500

    icon

---
+++ DerivedData/nds-clients/Index.noindex/Build/Intermediates.noindex/nds-clients.build/Debug-iphoneos/nds-clients.build/DerivedSources/CoreDataGenerated/nds_clients/nds_clients+CoreDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  nds_clients+CoreDataModel.swift
+//  
+//
+//  Created by Tyler M. Neher on 1/19/23.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
---
+++ DerivedData/nds-clients/Index.noindex/Build/Intermediates.noindex/nds-clients.build/Debug-iphonesimulator/nds-clients.build/DerivedSources/CoreDataGenerated/nds_clients/Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  
+//
+//  Created by Tyler M. Neher on 1/19/23.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
